b028672e136883c8fbe7a395287d9c853b198149
XavierChanth <xchanthavong@gmail.com>
2025-08-10 20:44:23 -0400

    fix: mobile layout

--- src/lib/components/PostListEntry.svelte
+++ src/lib/components/PostListEntry.svelte
@@ -7,12 +7,12 @@
 		class="border border-slate-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 bg-gray-50 hover:bg-white"
 	>
 		<a href={`/posts/${post.slug}`} class="block rounded p-6">
-			<div class="grid grid-cols-5 gap-4">
-				<div class="col-span-2">
+			<div class="grid grid-cols-1 md:grid-cols-5 gap-4">
+				<div class="col-span-1 md:col-span-2">
 					<h2 class="text-2xl font-semibold text-slate-900 mb-2">{post.title}</h2>
 					<p class="text-sm text-slate-600">{post.author} - {post.date}</p>
 				</div>
-				<div class="col-span-3">
+				<div class="col-span-1 md:col-span-3">
 					<p class="text-lg text-slate-700">{post.description}</p>
 				</div>
 			</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+
+	let isMenuOpen = $state(false);
+
+	function toggleMenu() {
+		isMenuOpen = !isMenuOpen;
+	}
 </script>
 
 <main class="min-h-screen bg-gray-100 flex flex-col">
-	<nav class="bg-white shadow-lg">
+
+	<nav class="bg-white shadow-lg sticky top-0">
 		<div class="max-w-6xl mx-auto px-4">
 			<div class="flex justify-between items-center h-16">
 				<div class="text-xl font-bold text-gray-800">
 					<a href="/">XavierChanth</a>
 				</div>
+        <!-- Desktop -->
 				<div class="hidden md:flex space-x-4">
 					<!-- TODO: highlight active -->
 					<a href="/" class="text-gray-600 hover:text-gray-900">About</a>
@@ -23,10 +31,33 @@
 					</a>
 					<a href="/posts" class="text-gray-600 hover:text-gray-900">Posts</a>
 				</div>
+        <!-- Mobile -->
+				<div class="md:hidden">
+					<button on:click={toggleMenu} class="text-gray-600 hover:text-gray-900 focus:outline-none">
+						<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							{#if isMenuOpen}
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+							{:else}
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+							{/if}
+						</svg>
+					</button>
+				</div>
 			</div>
+      {#if isMenuOpen}
+        <div class="flex flex-col justify-items-center text-center gap-4 pb-4">
+          <a href="/" class="text-gray-600 hover:text-blue-500 flex-col-1">About</a>
+          <a
+            href="https://github.com/xavierchanth"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-gray-600 hover:text-blue-500 flex-col-1"
+          >Projects</a>
+        <a href="/posts" class="text-gray-600 hover:text-blue-500 flex-col-1">Posts</a>
+        </div>
+      {/if}
 		</div>
 	</nav>
-
 	<div class="flex-grow">
 		{@render children()}
 	</div>
